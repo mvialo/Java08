321f76be40465fef87758526365c030cf411e1da
mvialo <manuella.vialo@outlook.fr>
2024-07-02 14:56:28 +0200

    Transfert sur le dépôt distant

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,5 @@
-public class Eagle extends Bird {
+
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -15,6 +16,44 @@ public class Eagle extends Bird {
 
     public boolean isFlying() {
         return flying;
+    }
+
+    @Override
+    public void takeOff() {
+        if (this.getAltitude() == 0 && this.isFlying() == false) {
+            this.flying = true;
+            System.out.println(this.getName() + " takes off in the sky.");
+        } else {
+            System.out.println(this.getName() + "is already flying");
+        }
+    }
+
+    @Override
+    public void ascend(int addedAltitude) {
+        this.altitude = altitude + addedAltitude;
+        System.out.println(this.getName() + " flies upward, altitude : " + String.valueOf(this.getAltitude()));
+    }
+
+    @Override
+    public void glide() {
+        System.out.println("glides into the air.");
+    }
+
+    @Override
+    public void descend(int addedAltitude) {
+        this.altitude = altitude - addedAltitude;
+        System.out.println(this.getName() + " flies downward, altitude : " + String.valueOf(this.getAltitude()));
+    }
+
+    @Override
+    public void land() {
+        if (this.altitude < 10) {
+            this.altitude = 0;
+            this.flying = false;
+            System.out.println(this.getName() + " lands on the ground.");
+        } else {
+            System.out.println(this.getName() + " is too high, it can't land.");
+        }
     }
 
     @Override
---
+++ Fly.java
@@ -0,0 +1,12 @@
+public interface Fly {
+    void takeOff();
+
+    void ascend(int altitude);
+
+    void glide();
+
+    void descend(int altitude);
+
+    void land();
+
+}
